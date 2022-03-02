ce08882f141156a78e93c2aa01dc62c3d4c69b74
chinwan16 <amit.chinwan.2019@mumail.ie>
2022-03-02 09:44:43 +0000

    heatmap viz for different measures

---
+++ R/heatmap_measures_Viz.R
@@ -0,0 +1,13 @@
+heatmap_measures_Viz <- function(association_measures) {
+
+  all_measures <- association_measures
+  all_measures$var3 <- paste0(all_measures$var1,"Vs",all_measures$var2)
+  all_measures <- all_measures[,-c(1,2)]
+
+  all_measure_longer <- tidyr::pivot_longer(all_measures,1:length(all_measures)-1,names_to = "measure",
+                                     values_to = "value")
+  ggplot2::ggplot(data=all_measure_longer,ggplot2::aes(x=measure,y=var3)) +
+    ggplot2::geom_tile(ggplot2::aes(fill=value)) +
+    ggplot2::scale_fill_gradientn(limits=c(-1,1),colors=c("red","white","blue"))
+
+}
