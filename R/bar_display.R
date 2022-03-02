02fa544b5d1120bd333edcd4832eb6ab4bf4a282
chinwan16 <amit.chinwan.2019@mumail.ie>
2022-03-02 09:12:34 +0000

    diff measures on upper+lower diag

--- R/bar_display.R
+++ R/bar_display.R
@@ -11,33 +11,65 @@
 #' bar_display(iris,group_by = "Species",method = "distance")
 
 
-bar_display <- function(association_measure) {
+bar_display <- function(lower_tri.measure, upper_tri.measure=NULL) {
 
-  m <- association_measure
-  upper_tri <- dplyr::filter(m,group_by != "overall")
-  lower_tri <- data.frame(var1 = upper_tri$var2,
-                          var2 = upper_tri$var1,
-                          measure = upper_tri$measure,
-                          group_by = upper_tri$group_by)
+  if(is.null(upper_tri.measure)){
 
-  cor_df <- rbind(upper_tri,lower_tri)
+    m <- lower_tri.measure
+    lower_tri <- dplyr::filter(m,group_by != "overall")
+    upper_tri <- lower_tri
+    names(upper_tri)[1:2] <- c("var2","var1")
 
-  overall_cor <- dplyr::filter(m,group_by == "overall")$measure
+    cor_df <- rbind(upper_tri,lower_tri)
 
-  cor_df$overall_cor <- overall_cor
-
-  ggplot2::ggplot(cor_df) +
-    ggplot2::facet_grid(ggplot2::vars(var1), ggplot2::vars(var2)) +
-    ggplot2::geom_bar(ggplot2::aes(x=1,y=measure,fill=group_by),stat = "identity",position = "dodge") +
-    ggplot2::geom_bar(ggplot2::aes(x=1,y=overall_cor),stat="identity",alpha=0.2,fill="tan1") +
-    ggplot2::scale_y_continuous(limits=c(-1,1)) +
-    ggplot2::theme(axis.text.x = ggplot2::element_blank(),
-                   panel.background = ggplot2::element_rect(),
-                   panel.grid.major = ggplot2::element_blank(),
-                   panel.grid.minor = ggplot2::element_blank(),
-                   axis.ticks.x = ggplot2::element_blank(),
-                   legend.position = "bottom")
+    lower_tri.overall_cor <- dplyr::filter(m,group_by == "overall")
+    upper_tri.overall_cor <- lower_tri.overall_cor
+    names(upper_tri.overall_cor)[1:2] <- c("var2","var1")
 
 
+    overall_cor <- rbind(upper_tri.overall_cor,lower_tri.overall_cor)
 
-}
+    ggplot2::ggplot(cor_df) +
+      ggplot2::facet_grid(ggplot2::vars(var1), ggplot2::vars(var2)) +
+      ggplot2::geom_bar(ggplot2::aes(x=1,y=measure,fill=group_by),stat = "identity",position = "dodge") +
+      ggplot2::geom_bar(data=overall_cor,ggplot2::aes(x=1,y=measure),stat="identity",alpha=0,color="black") +
+      ggplot2::scale_y_continuous(limits=c(-1,1)) +
+      ggplot2::theme(axis.text.x = ggplot2::element_blank(),
+                     panel.background = ggplot2::element_rect(),
+                     panel.grid.major = ggplot2::element_blank(),
+                     panel.grid.minor = ggplot2::element_blank(),
+                     axis.ticks.x = ggplot2::element_blank(),
+                     legend.position = "bottom")
+    }else{
+
+    m1 <- dplyr::filter(lower_tri.measure,group_by != "overall")
+    m2 <- dplyr::filter(upper_tri.measure,group_by != "overall")
+    lower_tri <- m1
+    upper_tri <- lower_tri
+    names(upper_tri)[1:2] <- c("var2","var1")
+    upper_tri$measure <- m2$measure
+
+    cor_df <- rbind(upper_tri,lower_tri)
+
+    m1_overall <- dplyr::filter(lower_tri.measure,group_by == "overall")
+    m2_overall <- dplyr::filter(upper_tri.measure,group_by == "overall")
+    lower_tri.overall_cor <- m1_overall
+    upper_tri.overall_cor <- lower_tri.overall_cor
+    names(upper_tri.overall_cor)[1:2] <- c("var2","var1")
+    upper_tri.overall_cor$measure <- m2_overall$measure
+
+    overall_cor <- rbind(upper_tri.overall_cor,lower_tri.overall_cor)
+
+    ggplot2::ggplot(cor_df) +
+      ggplot2::facet_grid(ggplot2::vars(var1), ggplot2::vars(var2)) +
+      ggplot2::geom_bar(ggplot2::aes(x=1,y=measure,fill=group_by),stat = "identity",position = "dodge") +
+      ggplot2::geom_bar(data=overall_cor,ggplot2::aes(x=1,y=measure),stat="identity",alpha=0,color="black") +
+      ggplot2::scale_y_continuous(limits=c(-1,1)) +
+      ggplot2::theme(axis.text.x = ggplot2::element_blank(),
+                     panel.background = ggplot2::element_rect(),
+                     panel.grid.major = ggplot2::element_blank(),
+                     panel.grid.minor = ggplot2::element_blank(),
+                     axis.ticks.x = ggplot2::element_blank(),
+                     legend.position = "bottom")
+    }
+  }
