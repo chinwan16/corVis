ff3ee92bb690cd45f769c124b79027629714da88
chinwan16 <amit.chinwan.2019@mumail.ie>
2022-03-10 09:39:09 +0000

    updating functions

--- R/association_measures.R
+++ R/association_measures.R
@@ -1,3 +1,17 @@
+#' Calculates the measures of association (pearson, kendall, spearman, distance and mic)
+#' between all variable pairs grouped by a grouping variable
+#'
+#' creates a dataframe with variable pairs and all the measures of association
+#'
+#' @param data dataframe
+#' @param group_by character vector specifying the grouping variable
+#' @param measures character vector specifying measures of association
+#'
+#' @return dataframe
+#' @export
+#'
+#' @examples
+#' association_measures(data=iris, group_by="Species)
 association_measures <- function(data=data,
                                  group_by=NULL,
                                  measures=c("pearson","spearman","kendall","distance","mic")){
--- R/heatmap_measures_Viz.R
+++ R/heatmap_measures_Viz.R
@@ -1,13 +1,41 @@
-heatmap_measures_Viz <- function(association_measures) {
+#' Display a linear layout plot for the measures of association
+#' between all the pairs of variables
+#'
+#' creates a linear plot of all the variable pairs and measures of association in the form of a heatmap
+#'
+#' @param association_measures dataframe
+#'
+#' @return plot
+#' @export
+#'
+#' @examples
+#' heatmap_mesaures_Viz(association_measures)
+#'
+heatmap_measures_Viz <- function(association_measures,group_by=NULL) {
 
-  all_measures <- association_measures
-  all_measures$var3 <- paste0(all_measures$var1,"Vs",all_measures$var2)
-  all_measures <- all_measures[,-c(1,2)]
+  if(is.null(group_by)){
+    all_measures <- association_measures
+    all_measures$var3 <- paste0(all_measures$var1,"Vs",all_measures$var2)
+    all_measures <- all_measures[,-c(1,2)]
 
-  all_measure_longer <- tidyr::pivot_longer(all_measures,1:length(all_measures)-1,names_to = "measure",
-                                     values_to = "value")
-  ggplot2::ggplot(data=all_measure_longer,ggplot2::aes(x=measure,y=var3)) +
-    ggplot2::geom_tile(ggplot2::aes(fill=value)) +
-    ggplot2::scale_fill_gradientn(limits=c(-1,1),colors=c("blue","white","red"))
+    all_measure_longer <- tidyr::pivot_longer(all_measures,1:length(all_measures)-1,names_to = "measure",
+                                              values_to = "value")
+    ggplot2::ggplot(data=all_measure_longer,ggplot2::aes(x=measure,y=var3)) +
+      ggplot2::geom_tile(ggplot2::aes(fill=value)) +
+      ggplot2::scale_fill_gradientn(limits=c(-1,1),colors=c("blue","white","red"))
+
+  }else{
+    all_measures <- association_measures
+    all_measures$var3 <- paste0(all_measures$var1,"Vs",all_measures$var2)
+    all_measures <- all_measures[,-c(1,2)]
+    keep <- c("var3","group_by")
+
+    all_measure_longer <- tidyr::pivot_longer(all_measures,-all_of(keep),names_to = "measure",
+                                              values_to = "value")
+    ggplot2::ggplot(data=all_measure_longer,ggplot2::aes(x=measure,y=var3)) +
+      ggplot2::geom_tile(ggplot2::aes(fill=value)) +
+      ggplot2::scale_fill_gradientn(limits=c(-1,1),colors=c("blue","white","red")) +
+      ggplot2::facet_wrap(~group_by)
+  }
 
 }
