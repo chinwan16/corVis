c8956a865e9d0936aada7eb51e5710d92338b431
chinwan16 <amit.chinwan.2019@mumail.ie>
2022-03-23 08:27:21 +0000

    ordering with barplot display

--- R/bar_display.R
+++ R/bar_display.R
@@ -11,7 +11,47 @@
 #' bar_display(lower_tri)
 
 
-bar_display <- function(lower_tri.measure, upper_tri.measure=NULL) {
+bar_display <- function(lower_tri.measure, upper_tri.measure=NULL,order.by="overall") {
+
+  if(order.by=="overall"){
+    names(lower_tri.measure)[3] <- "measure"
+    m <- lower_tri.measure
+    cor_df_overall <- dplyr::filter(m,group_by == "overall")
+
+    uni <- unique(unlist(cor_df_overall[,1:2]))
+    m1 <- matrix(0, nrow=length(uni), ncol=length(uni),dimnames=list(uni, uni))
+    m1[cbind(match(cor_df_overall$var1, rownames(m1)), match(cor_df_overall$var2, colnames(m1)))] <-
+      cor_df_overall$measure
+
+    cor_matrix <- m1 + t(m1)
+    cor_matrix_n <- 1-cor_matrix
+    diag(cor_matrix_n) <- 0
+    d <- stats::dist(cor_matrix_n)
+    clust <- stats::hclust(d)
+    ordering <- rownames(cor_matrix)[clust$order]
+
+  }else if(order.by=="max_diff"){
+
+    names(lower_tri.measure)[3] <- "measure"
+    m <- lower_tri.measure
+
+    df_max_diff <- dplyr::group_by(m,var1,var2)
+    df_max_diff <- dplyr::mutate(df_max_diff,max_diff=diff(range(measure)))
+    df_max_diff_uni <- unique.data.frame(df_max_diff[,c("var1","var2","max_diff")])
+
+    uni <- unique(unlist(df_max_diff_uni[,1:2]))
+    m1 <- matrix(0, nrow=length(uni), ncol=length(uni),dimnames=list(uni, uni))
+    m1[cbind(match(df_max_diff_uni$var1, rownames(m1)), match(df_max_diff_uni$var2, colnames(m1)))] <-
+      df_max_diff_uni$max_diff
+
+    cor_matrix <- m1+t(m1)
+    cor_matrix_n <- 1-cor_matrix
+    diag(cor_matrix_n) <- 0
+    d <- stats::dist(cor_matrix_n)
+    clust <- stats::hclust(d)
+    ordering <- rownames(cor_matrix)[clust$order]
+
+  }
 
 
   if(is.null(upper_tri.measure)){
@@ -22,6 +62,8 @@ bar_display <- function(lower_tri.measure, upper_tri.measure=NULL) {
     names(upper_tri)[1:2] <- c("var2","var1")
 
     cor_df <- rbind(upper_tri,lower_tri)
+    cor_df$var1 <- factor(cor_df$var1,levels=ordering)
+    cor_df$var2 <- factor(cor_df$var2,levels=ordering)
 
     lower_tri.overall_cor <- dplyr::filter(m,group_by == "overall")
     upper_tri.overall_cor <- lower_tri.overall_cor
@@ -29,6 +71,8 @@ bar_display <- function(lower_tri.measure, upper_tri.measure=NULL) {
 
 
     overall_cor <- rbind(upper_tri.overall_cor,lower_tri.overall_cor)
+    overall_cor$var1 <- factor(overall_cor$var1,levels=ordering)
+    overall_cor$var2 <- factor(overall_cor$var2,levels=ordering)
 
     ggplot2::ggplot(cor_df) +
       ggplot2::facet_grid(ggplot2::vars(var1), ggplot2::vars(var2)) +
