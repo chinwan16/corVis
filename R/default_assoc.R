2750e796f62c4285dc4e002e723e3f653c709a70
chinwan16 <amit.chinwan.2019@mumail.ie>
2022-05-26 09:22:47 +0100

    vignette update

---
+++ R/default_assoc.R
@@ -0,0 +1,22 @@
+#' A user defined function for association measures
+#'
+#' Creates a tibble for different measures of association for different variable types of a dataset.
+#'
+#' @return tibble
+#' @export
+#'
+#' @examples
+#' default_assoc()
+#' spearman_assoc <- default_assoc()
+#' spearman_assoc$argList[[1]] <- list(method="spearman")
+#' spearman_assoc()
+
+
+default_assoc <- function(){
+  tribble(
+    ~funName, ~typeX, ~typeY, ~argList,
+    "tbl_cor", "numeric", "numeric", NULL,
+    "tbl_tau", "ordered", "ordered", NULL,
+    "tbl_cancor",  "factor", "numeric", NULL,
+    "tbl_cancor", "other", "other",NULL)
+}
