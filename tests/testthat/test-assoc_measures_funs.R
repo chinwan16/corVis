0c5590770cdb96790d990390a236a6dbee1e15df
chinwan16 <amit.chinwan.2019@mumail.ie>
2022-07-05 08:29:03 +0100

    unit tests

--- tests/testthat/test-assoc_measures_funs.R
+++ tests/testthat/test-assoc_measures_funs.R
@@ -18,7 +18,7 @@ test_that("all tbl_* functions work", {
 
   #tbl_dcor (distance correlation)
   distance <- tbl_dcor(df)
-  expect_s3_class(can_cor,c("pairwise","tbl_df","tbl","data.frame"))
+  expect_s3_class(distance,c("pairwise","tbl_df","tbl","data.frame"))
 
   #tbl_easy (easystats package measures)
   easystats_per <- tbl_easy(df,method = "percentage")
@@ -45,6 +45,73 @@ test_that("all tbl_* functions work", {
   #tbl_uncertainty (Uncertainty coefficient)
   uncertainty <- tbl_uncertainty(df)
   expect_s3_class(uncertainty,c("pairwise","tbl_df","tbl","data.frame"))
+
+
+})
+
+test_that("tbl_* functions return a value less than or equal to 1 ", {
+
+  df <- iris
+
+  #tbl_cor
+  pearson <- tbl_cor(df)
+  expect_lte(sample(pearson$measure,1),1)
+
+  spearman <- tbl_cor(df, method = "spearman")
+  expect_lte(sample(spearman$measure,1),1)
+
+  kendall <- tbl_cor(df, method = "kendall")
+  expect_lte(sample(kendall$measure,1),1)
+
+  #tbl_cancor (canonical correlation)
+  can_cor <- tbl_cancor(df)
+  expect_lte(sample(can_cor$measure,1),1)
+
+  #tbl_dcor (distance correlation)
+  distance <- tbl_dcor(df)
+  expect_lte(sample(distance$measure,1),1)
+
+})
+
+
+test_that("tbl_cor functions return a value greater than or equal to -1",{
+
+  df <- iris
+
+  #tbl_cor
+  pearson <- tbl_cor(df)
+  expect_gte(sample(pearson$measure,1),-1)
+
+  spearman <- tbl_cor(df, method = "spearman")
+  expect_gte(sample(spearman$measure,1),-1)
+
+  kendall <- tbl_cor(df, method = "kendall")
+  expect_gte(sample(kendall$measure,1),-1)
+
+
+})
+
+
+test_that("tbl_dcor/tbl_cancor/tbl_mine/tbl_tau functions return a value greater than or equal to 0",{
+
+  df <- iris
+
+  #tbl_cancor (canonical correlation)
+  can_cor <- tbl_cancor(df)
+  expect_gte(sample(can_cor$measure,1),0)
+
+  #tbl_dcor (distance correlation)
+  distance <- tbl_dcor(df)
+  expect_gte(sample(distance$measure,1),0)
+
+
+  #tbl_mine (mine measures)
+  mine_mic <- tbl_mine(df,method = "mic")
+  expect_gte(sample(mine_mic$measure,1),0)
+
+  #tbl_tau (Kendall Tau coefficients from DescTools package)
+  KendallTauA <- tbl_tau(df, method = "A")
+  expect_gte(sample(KendallTauA$measure,1),0)
 
 
 })
--- tests/testthat/test-calculate_association.R
+++ tests/testthat/test-calculate_association.R
@@ -1,3 +1,60 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("calc_assoc function returns object with correct class", {
+
+  df <- iris
+
+  updated_assoc <- update_assoc(num_pair = "tbl_nmi")
+  assoc <- calc_assoc(iris,types = updated_assoc)
+  expect_s3_class(assoc,c("pairwise","tbl_df","tbl","data.frame"))
+
+  updated_assoc <- update_assoc(mixed_pair = "tbl_uncertainty")
+  assoc <- calc_assoc(iris,types = updated_assoc)
+  expect_s3_class(assoc,c("pairwise","tbl_df","tbl","data.frame"))
+
+})
+
+
+test_that("calc_assoc function returns measures value less than 1", {
+
+  df <- iris
+
+  updated_assoc <- update_assoc(num_pair = "tbl_nmi")
+  assoc <- calc_assoc(iris,types = updated_assoc)
+  expect_lte(sample(assoc$measure,1),1)
+
+  updated_assoc <- update_assoc(mixed_pair = "tbl_uncertainty")
+  assoc <- calc_assoc(iris,types = updated_assoc)
+  expect_lte(sample(assoc$measure,1),1)
+
+
+})
+
+
+test_that("calc_assoc_by function returns object with correct class", {
+
+  df <- iris
+
+  updated_assoc <- update_assoc(num_pair = "tbl_nmi")
+  assoc_by <- calc_assoc_by(iris,types = updated_assoc,by="Species")
+  expect_s3_class(assoc_by,c("pairwise","tbl_df","tbl","data.frame"))
+
+  updated_assoc <- update_assoc(num_pair_argList = "kendall")
+  assoc_by <- calc_assoc_by(iris,types = updated_assoc,by="Species")
+  expect_s3_class(assoc_by,c("pairwise","tbl_df","tbl","data.frame"))
+
+})
+
+
+test_that("calc_assoc_by function returns measures value less than 1", {
+
+  df <- iris
+
+  updated_assoc <- update_assoc(num_pair = "tbl_nmi")
+  assoc_by <- calc_assoc_by(iris,types = updated_assoc,by="Species")
+  expect_lte(sample(assoc_by$measure,1),1)
+
+  updated_assoc <- update_assoc(mixed_pair = "tbl_uncertainty")
+  assoc_by <- calc_assoc_by(iris,types = updated_assoc,by="Species")
+  expect_lte(sample(assoc_by$measure,1),1)
+
+
 })
